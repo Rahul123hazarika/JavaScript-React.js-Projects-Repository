79ba9bbaf25b5d674e9c691425bd644eb0145172
Rahul123hazarika <98834787+Rahul123hazarika@users.noreply.github.com>
2025-09-19 12:46:50 +0530

    Functions & arrow functions, template literals.js

---
+++ Functions & arrow functions, template literals.js
@@ -0,0 +1,9 @@
+function add(a,b){
+    return a+b;
+}
+const multiply=()=>a*b; // using arrow function
+const greet= name=>'hello , ${name}'; //// here name is like a value or parameter greet(name) 
+console.log(add(2,3));
+console.log(add(2,3));
+console.log(multiply(4,5));
+console.log(greet("rahul"));
