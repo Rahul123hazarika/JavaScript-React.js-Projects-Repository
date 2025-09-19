a46a1094109a376726874d7c327457e3ac9f67e2
Rahul123hazarika <98834787+Rahul123hazarika@users.noreply.github.com>
2025-09-19 18:29:01 +0530

    Create Arrays & objects.js

---
+++ Arrays & objects.js
@@ -0,0 +1,8 @@
+const arr=[1,2,3];
+arr.push(4);
+console.log(arr);
+const person={name:"Rahul", age:22}
+console.log(person.name, person.age);
+for(let i=0;i<arr.length;i++){
+    console.log(arr[i]);
+} 
